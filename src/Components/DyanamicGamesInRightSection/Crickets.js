213b01464f215ee59b202bab863b5273a3b4c020
AmanAsharma070320 <asharma070320@gmail.com>
2024-12-09 15:10:50 +0530

    Gaming Project

--- src/Components/DyanamicGamesInRightSection/Crickets.js
+++ src/Components/DyanamicGamesInRightSection/Crickets.js
@@ -22,6 +22,274 @@ const Crickets = () => {
     { match: "West Indies v Bangladesh", date: "08/12/2024", time: "07:00:00 PM" },
   ];
 
+  const showGames = [
+    {
+        "name": "MINI SUPER OVER",
+        "image": "https://dataobj.ecoassetsservice.com/casino-icons/lc/goal.jpg"
+    },
+    {
+        "name": "GOAL",
+        "image": "https://dataobj.ecoassetsservice.com/casino-icons/lc/goal.jpg"
+    },
+    {
+        "name": "ANDAR BAZAR",
+        "image": "https://dataobj.ecoassetsservice.com/casino-icons/lc/goal.jpg"
+    },
+    {
+        "name": "130 CARDS",
+        "image": "https://dataobj.ecoassetsservice.com/casino-icons/lc/goal.jpg"
+    },
+    {
+        "name": "LUCKY 15",
+        "image": "https://dataobj.ecoassetsservice.com/casino-icons/lc/goal.jpg"
+    },
+    {
+        "name": "SUPER OVER2",
+        "image": "https://dataobj.ecoassetsservice.com/casino-icons/lc/superover2.jpg"
+    },
+    {
+        "name": "TEENPATTI",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "TEENPATTI 20-20",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "TEENPATTI TEST",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "TEENPATTI OPEN",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "POKER",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "1-DAY TEENPATTI",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "INSTANT",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "SIC BO",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "BALL BY BALL",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "INSTANT 2.0",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "ROULETTE",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "TEENPATTI 20-20",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "TEENPATTI 20-20",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "TEENPATTI TEST",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "TEENPATTI OPEN",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "POKER",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "POKER 6 PLAYERS",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "BACCARAT",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "ONLINE BACCARAT",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "20-20 DRAGON TIGER",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "1 DAY DRAGON TIGER",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "20-20 TIGER",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "20-20 DRAGON",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "32 CARDS RUMMY",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "22 CARDS RUMMY",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "3 CARDS JOKIWILD",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "CASINO WAR",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "WORLD MATKA",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "INSTANT WORLD",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "ANDAR BAZAR",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "BOLLYWOOD CASINO",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "LOTTERY",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "CRICKET LIVE",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "CRICKET MATCH 20-20",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "CASINO METER",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "TEENPATTI 20-20",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "QUEEN",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "RACE",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "LUCKY 7-C",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "SUPER OVER",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+        "name": "THE TRAP",
+        "image": "https://i.imgur.com/c59439a.png"
+    },
+    {
+      "name": "2 Cards Teenpatti",
+      "image": "https://i.imgur.com/c59439a.png" 
+  },
+  {
+      "name": "Muflis Teenpatti",
+      "image": "https://i.imgur.com/c59439a.png"
+  },
+  {
+      "name": "20-20 Teenpatti",
+      "image": "https://i.imgur.com/c59439a.png"
+  },
+  {
+      "name": "Teenpatti",
+      "image": "https://i.imgur.com/c59439a.png"
+  },
+  {
+      "name": "2 Cards Teenpatti",
+      "image": "https://i.imgur.com/c59439a.png"
+  },
+  {
+      "name": "20Card Baccarat",
+      "image": "https://i.imgur.com/c59439a.png"
+  },
+  {
+      "name": "Muflis Teenpatti",
+      "image": "https://i.imgur.com/c59439a.png"
+  },
+  {
+      "name": "Race to 17",
+      "image": "https://i.imgur.com/c59439a.png"
+  },
+  {
+      "name": "20-20 Teenpatti",
+      "image": "https://i.imgur.com/c59439a.png"
+  },
+  {
+      "name": "Trio",
+      "image": "https://i.imgur.com/c59439a.png"
+  },
+  {
+      "name": "Note Number",
+      "image": "https://i.imgur.com/c59439a.png"
+  },
+  {
+      "name": "Kaun Banega Crorepati",
+      "image": "https://i.imgur.com/c59439a.png"
+  },
+  {
+      "name": "1 Card 20-20",
+      "image": "https://i.imgur.com/c59439a.png"
+  },
+  {
+      "name": "1 Card One-Day",
+      "image": "https://i.imgur.com/c59439a.png"
+  },
+  {
+      "name": "Roulette",
+      "image": "https://i.imgur.com/c59439a.png"
+  },
+ 
+  {
+      "name": "Race to 2nd",
+      "image": "https://i.imgur.com/c59439a.png"
+  },
+  {
+      "name": "Instant Teenpatti",
+      "image": "https://i.imgur.com/c59439a.png"
+  },
+  {
+      "name": "Dus Ka Dum",
+      "image": "https://i.imgur.com/c59439a.png"
+  },
+  {
+      "name": "1 Card Meter",
+      "image": "https://i.imgur.com/c59439a.png"
+  }
+]
+
   const activeCircleIndices = [0, 1, 2, 3, 4, 5,6,7];
   const televisionIndices = [0, 1, 2, 3, 4, 5, 8, 11, 14, 15];
   const fIconIndices = [0, 1, 2, 3, 4, 5, 9,11];
@@ -98,6 +366,17 @@ const Crickets = () => {
             </div>
           </div>
         ))}
+      </div>
+      {/* show all games */}
+      <div className="showAll_games">
+        {
+          showGames.map((games,idx)=>{
+            return <div className="games_img" key={idx}>
+              <img src={games.image} alt="game_img" />
+              <p>{games.name}</p>
+            </div>
+          })
+        }
       </div>
     </div>
   );
